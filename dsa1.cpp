3d0ed406748bebc85834bb438039e84aa4e7c988
ricky kumar <rickykumarmth000@gmail.com>
2026-01-26 21:32:02 +0530

    only i check whether the file ssave o rnot

---
+++ dsa1.cpp
@@ -0,0 +1,9 @@
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "This is a sample C++ program." << endl;
+    cout<< "C++17 features can be used in this project." << endl;
+    return 0;
+}
