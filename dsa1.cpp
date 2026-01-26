fe5553b6467e529525c0d749b446dc4d2855506c
ricky kumar <rickykumarmth000@gmail.com>
2026-01-26 23:22:14 +0530

    optinal partition of string

--- dsa1.cpp
+++ dsa1.cpp
@@ -1,9 +1,28 @@
 
+// optial partition of string
 #include<iostream>
+#include<unordered_set>
+#include<string>
 using namespace std;
+
+int optimal_partition_of_String(string s){
+  unordered_set<char> hash;
+    int count=1;
+    for(char c:s){
+        if(hash.find(c)!=hash.end()){ // element found in hash
+            count++;
+            hash.clear();
+            hash.insert(c);
+        }
+        else{
+            hash.insert(c);
+        }
+      
+    }
+    return count;
+}
 int main() {
-    cout << "Hello, World!" << endl;
-    cout << "This is a sample C++ program." << endl;
-    cout<< "C++17 features can be used in this project." << endl;
+    string s="Dataeee";
+    cout<<"Total number of OPtimal partition string is: "<< optimal_partition_of_String(s)<<endl;
     return 0;
 }
